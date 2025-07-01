Fix sign error in LJ force

pyhnc/potentials.py
@@ -245,7 +245,7 @@ class LennardJones(Potential):
 
         rinv = self.sigma/r
         r6inv = rinv**6
-        f = -4*self.epsilon * (12*r6inv**2 - 6*r6inv) / r
+        f = 4*self.epsilon * (12*r6inv**2 - 6*r6inv) / r
         f[r >= self.rcut] = 0.
 
         f = np.squeeze(f)
@@ -279,7 +279,7 @@ def test_lj():
     test_copy(v, pickle.loads(pickle.dumps(v)))
 
     from scipy.optimize import approx_fprime
-    exact = np.array([approx_fprime(rr, v.potential) for rr in r]).reshape(-1)
+    exact = np.array([-approx_fprime(rr, v.potential) for rr in r]).reshape(-1)
     assert np.allclose(v.force(r), exact, rtol=1e-6)
 
     rcut = 2.