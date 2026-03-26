6317ddd0db21eb6d05b6afb8bc2833ee4b8e44b2
Patrick B Warren <patrickbwarren@gmail.com>
2026-03-26 09:35:22 +0000

    replace tranz by simpson

--- dpd_eos.py
+++ dpd_eos.py
@@ -26,25 +26,32 @@ import numpy as np
 import pandas as pd
 from numpy import pi as π
 from pyHNC import truncate_to_zero
+from scipy.integrate import simpson
 
 parser = argparse.ArgumentParser(description='DPD EoS calculator')
 pyHNC.add_grid_args(parser)
 pyHNC.add_solver_args(parser)
 parser.add_argument('-A', '--A', default='10(10)50', help='repulsion amplitude range')
 parser.add_argument('-r', '--rho', default='1(1)10', help='density range')
+parser.add_argument('-v', '--verbose', action='count', default=0)
 args = parser.parse_args()
 
 grid = pyHNC.Grid(**pyHNC.grid_args(args)) # make the initial working grid
-r, Δr = grid.r, grid.deltar # extract the co-ordinate array for use below
+
+r = grid.r
 
 solver = pyHNC.PicardHNC(grid, **pyHNC.solver_args(args))
+
+if args.verbose:
+    print(grid.details)
+    print(solver.details)
 
 # DPD potential and force law omitting amplitude;
 # the array sizes here are ng-1, same as r[:].
 
 φbyA = truncate_to_zero(1/2*(1-r)**2, r, 1)
 fbyA = truncate_to_zero((1-r), r, 1)
-w = 15/π * φbyA # normalised weight function
+w = 30/π * truncate_to_zero(1/4*(1-r)**2, r, 1) # normalised weight function
 
 # The virial pressure, p = ρ + 2πρ²/3 ∫_0^∞ dr r³ f(r) g(r) where
 # f(r) = −d φ/dr is the force.  See Eq. (2.5.22) in Hansen & McDonald,
@@ -53,7 +60,7 @@ w = 15/π * φbyA # normalised weight function
 # The constant term is the mean field contribution, namely
 # 2πρ²/3 ∫_0^∞ dr r³ f(r) = A ∫_0^1 dr r³ (1−r) = πAρ²/30.
 
-# We calculate also <n> = 4π ∫_0^∞ dr r² w(r) g(r).
+# We calculate also <n> = 4πρ ∫_0^∞ dr r² w(r) g(r).
 
 data = [] # this will grow as computations proceed
 
@@ -62,8 +69,8 @@ for A in pyHNC.as_linspace(args.A):
     for ρ in pyHNC.as_linspace(args.rho):
         h = solver.solve(A*φbyA, ρ).hr # just keep h(r)
         if solver.converged: # but do test if converged !
-            pexbyA = π*ρ**2/30 + 2*π*ρ**2/3 * np.trapz(r**3*fbyA*h, dx=Δr)
-            ζav = 4*π * np.trapz(r**2*w*h, dx=Δr) # may notation-clash with mbdpd codes
+            pexbyA = π*ρ**2/30 + 2*π*ρ**2/3 * simpson(r**3*fbyA*h, r)
+            ζav = 4*π * simpson(r**2*w*h, r) # may notation-clash with mbdpd codes
             nav = ρ*(1 + ζav) # the mean local density
             p = ρ + A*pexbyA
             data.append((A, ρ, ρ**2, p, pexbyA, ζav, nav, solver.error))
